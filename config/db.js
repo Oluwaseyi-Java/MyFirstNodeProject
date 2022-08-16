dc0d44b0784a705ec971a437b23dd57c3654ed8a
Adesunloro Oluwaseyi <amiltontech.co@gmail.com>
2022-08-16 13:36:00 +0100

    Enabled database

---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // mongodb + srv://Adeseyi:<password>@portfolio.bs2nhbj.mongodb.net/?retryWrites=true&w=majority
+    url: "srv://Adeseyi:Adeseyi18@portfolio.bs2nhbj.mongodb.net/?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -1,53 +1,78 @@
 const express = require("express")
-const MongoClient = require("mongodb").MongoClient
-// const bodyParser = require("body-parser")
-
-const cors = require("cors")
-const { json, urlencoded } = require('express')
 const app = express()
+const { json, urlencoded } = require('express')
+const mongoose = require('mongoose')
+const Review = require("./app/models/Review")
+const PORT = process.env.PORT || 4000
+const dotenv = require('dotenv')
+const cors = require('cors')
+const dbURL = 'mongodb+srv://portfolio-review:myAdeseyiReview@portfolio.bs2nhbj.mongodb.net/reviews?retryWrites=true&w=majority'
 
-// app.use(bodyParser.urlencoded({ extended: true }))
-const Port = process.env.PORT || 4000
 
+dotenv.config()
+//connect to mogongodb
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => {
 
+        app.listen(PORT, () => {
+            console.log("We are live on port " + PORT)
+        })
+
+        console.log(result)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 app.use(cors())
 app.use(json())
-app.use(urlencoded({ extended: false }))
-
-
-
-
-require('./app/routes')(app, {})
-
-app.listen(Port, () => {
-    console.log("We are live on port " + Port)
-})
+app.use(urlencoded({ extended: true }))
 
 // This scetion were disabled because of untracked problem
-
-// app.use(urlencoded({ extended: false }))
-
 // // Add headers before the routes are defined
-// app.use(function (req, res, next) {
+app.use(function (req, res, next) {
+    console.log(req)
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://oluwaseyiportfolio.vercel.app');
 
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
 
-//     // Pass to next layer of middleware
-//     next();
-// });
-//
 // End of untracked problem
 
-// mongodb + srv://Adeseyi:<password>@portfolio.bs2nhbj.mongodb.net/?retryWrites=true&w=majority
+
+
+
+app.get('/', (req, res) => {
+    Review.find()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    const review = new Review(req.body.reviewData)
+    review.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            res.send(err)
+        })
+})
+
